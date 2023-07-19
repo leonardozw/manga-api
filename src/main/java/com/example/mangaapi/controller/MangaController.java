a741901c8007d6a1db6aa6d74cc98f8fa8988207
leonardo <leonardozw46@gmail.com>
2023-07-19 17:49:11 -0300

    feat: manga controller

---
+++ src/main/java/com/example/mangaapi/controller/MangaController.java
@@ -0,0 +1,46 @@
+package com.example.mangaapi.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mangaapi.entity.Manga;
+import com.example.mangaapi.service.MangaService;
+
+@RestController
+@RequestMapping("/mangas")
+public class MangaController {
+
+    private MangaService mangaService;
+
+    public MangaController(MangaService mangaService) {
+        this.mangaService = mangaService;
+    }
+
+    @PostMapping
+    Manga create(@RequestBody Manga manga) {
+        return mangaService.create(manga);
+    }
+
+    @GetMapping
+    List<Manga> list() {
+        return mangaService.list();
+    }
+
+    @PutMapping
+    Manga update(@RequestBody Manga manga) {
+        return mangaService.update(manga);
+    }
+
+    @DeleteMapping
+    List<Manga> delete(@PathVariable("id") Long id) {
+        return mangaService.delete(id);
+    }
+}
